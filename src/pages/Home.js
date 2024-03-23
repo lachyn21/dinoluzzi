98d01cb7f96aee32d9680a63d7ef40262c9e656f
darango3002 <donaldarango@ufl.edu>
2024-03-23 14:21:35 -0400

    added text to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,10 +8,16 @@ function Home() {
             <script id="ze-snippet" src="https://static.zdassets.com/ekr/snippet.js?key=c0a9e439-e854-4494-835e-75f303129020"></script>
             {/* End of chatbot script */}
 
-            
+
             <div className="left">
-                <h1 className="text1">DINO LUZZY</h1> 
+                <h1 className="text1">DINO LUZZI</h1> 
                 <h1 className="text2"> ENERGY DRINK</h1>
+                <div>
+                    <p>
+                    This delicious, healthful, energizing refresher made in Italy is distinguished from its more well-known competitors by its decided non-metallic after taste.
+                    </p>
+                </div>
+                
             </div>
             <div className="right">
                 <img src={DinoDrink} className="dino-drink" alt="DinoLuzziDrink"></img>
