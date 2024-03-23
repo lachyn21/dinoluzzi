ea71ba6feb6906b72bccd2b33d8ab1f9f77f19ba
darango3002 <donaldarango@ufl.edu>
2024-03-23 15:04:42 -0400

    added styling to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,11 +3,12 @@ import "../styles/home.css"
 
 function Home() {
     return (
-        <div className="container section-1">
-            {/* Start of chatbot script */}
-            <script id="ze-snippet" src="https://static.zdassets.com/ekr/snippet.js?key=c0a9e439-e854-4494-835e-75f303129020"></script>
-            {/* End of chatbot script */}
+        <>
+        {/* Start of chatbot script */}
+        <script id="ze-snippet" src="https://static.zdassets.com/ekr/snippet.js?key=c0a9e439-e854-4494-835e-75f303129020"></script>
+        {/* End of chatbot script */}
 
+        <div className="container section-1">
             <div className="left">
                 <div className="sec1-title">
                     <h1 className="text1">DINO LUZZI</h1> 
@@ -18,12 +19,22 @@ function Home() {
                     This delicious, healthful, energizing refresher made in Italy is distinguished from its more well-known competitors by its decided non-metallic after taste.
                     </p>
                 </div>
-                
+                <div className="sec1-p">
+                    <p>
+                    115 calories, 0 fat
+                    </p>
+                </div>
+                <div className="sec1-btn">
+                    <button className="btn btn-accent">
+                        Buy Now
+                    </button>
+                </div>
             </div>
             <div className="right">
                 <img src={DinoDrink} className="dino-drink" alt="DinoLuzziDrink"></img>
             </div>
         </div>
+        </>
     );
 }   
 
