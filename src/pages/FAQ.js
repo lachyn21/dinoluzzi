0df56d4a39d570117c4e882d3ec0a07d654a6ca3
Lachyn Almazova <lalmazova@ufl.edu>
2024-03-23 13:28:39 -0400

    added script to chatbot

--- src/pages/FAQ.js
+++ src/pages/FAQ.js
@@ -1,7 +1,12 @@
 function FAQ() {
     return (
-        <h1>FAQ</h1>
+        <>
+            <h1>FAQ</h1>
+            {/* Start of chatbot script */}
+            <script id="ze-snippet" src="https://static.zdassets.com/ekr/snippet.js?key=c0a9e439-e854-4494-835e-75f303129020"></script>
+            {/* End of chatbot script */}
+        </>
     );
-}   
+}
 
-export default FAQ
+export default FAQ;
--- src/pages/Locations.js
+++ src/pages/Locations.js
@@ -1,6 +1,11 @@
 function Locations() {
     return (
-        <h1>Locations</h1>
+        <>
+            <h1>Locations</h1>
+            {/* Start of chatbot script */}
+            <script id="ze-snippet" src="https://static.zdassets.com/ekr/snippet.js?key=c0a9e439-e854-4494-835e-75f303129020"></script>
+            {/* End of chatbot script */}
+        </>
     );
 }   
 
