0da8aa873ea8b89eccca92ccb395e90e2cbaec08
darango3002 <donaldarango@ufl.edu>
2024-03-23 14:36:53 -0400

    added style changes to home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,11 +8,12 @@ function Home() {
             <script id="ze-snippet" src="https://static.zdassets.com/ekr/snippet.js?key=c0a9e439-e854-4494-835e-75f303129020"></script>
             {/* End of chatbot script */}
 
-
             <div className="left">
-                <h1 className="text1">DINO LUZZI</h1> 
-                <h1 className="text2"> ENERGY DRINK</h1>
-                <div>
+                <div className="sec1-title">
+                    <h1 className="text1">DINO LUZZI</h1> 
+                    <h1 className="text2"> ENERGY DRINK</h1>
+                </div>  
+                <div className="sec1-p">
                     <p>
                     This delicious, healthful, energizing refresher made in Italy is distinguished from its more well-known competitors by its decided non-metallic after taste.
                     </p>
