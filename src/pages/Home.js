c37e2f0ec19e4b7e2cf0889e45944aaaacc9fecb
darango3002 <donaldarango@ufl.edu>
2024-03-23 20:53:27 -0400

    started in middle section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Divider from "../assets/divider.png";
 import DinoDrink from "../assets/dino-drink-liquid.png";
-import dialIcon from "../assets/dialIcon.png";
-import energyIcon from "../assets/energyIcon.png";
-import italyIcon from "../assets/italyIcon.png";
-import leafIcon from "../assets/leafIcon.png";
-import zeroIcon from "../assets/zeroIcon.png";
+import DialIcon from "../assets/dialIcon.png";
+import EnergyIcon from "../assets/energyIcon.png";
+import ItalyIcon from "../assets/italyIcon.png";
+import LeafIcon from "../assets/leafIcon.png";
+import ZeroIcon from "../assets/zeroIcon.png";
+import DinoSparkle from "../assets/dinoSparkle.png"
 import "../styles/home.css";
 
 function Home() {
@@ -13,7 +14,7 @@ function Home() {
         <>
         <div className="container section-1">
             <div className="left">
-                <div className="sec1-title">
+                <div className="sec1-title section-title">
                     <h1 className="text1">DINO LUZZI <span className="text2">ENERGY DRINK</span></h1>
                 </div>  
                 <div className="sec1-p">
@@ -33,42 +34,83 @@ function Home() {
                     </a>
                 </div>
             </div>
-            <div className="right">
-                <img src={DinoDrink} className="dino-drink" alt="DinoLuzziDrink"></img>
+            <div className="img-container">
+                <img src={DinoDrink} className="medium-image" alt="DinoLuzziDrink"></img>
             </div>
         </div>
         <div className='divider'>
             <img src={Divider} alt="DividerImage"></img>
         </div>
         <div className='container section-2'>
-            <div className='top'>
-                <div>
+            <div>
+                <div className='icon-container'>
+                    <img src={DialIcon} className='icon' alt='DialIcon'></img>
                     <h2>
                         Low Calories
                     </h2>
                 </div>
-                <div>
+                <div className='icon-container'>
+                    <img src={EnergyIcon} className='icon' alt='EnergyIcon'></img>
                     <h2>
                         Maximum Energy
                     </h2>
                 </div>
-                <div>
+                <div className='icon-container'>
+                    <img src={LeafIcon} className='icon' alt='LeafIcon'></img>
                     <h2>
                         Smart Organics
-                    </h2> 
+                    </h2>
                 </div>
             </div>
-            <div className='bottom'>
-                <div>
-                    <img src={zeroIcon} className='icon'></img>
+            <div>
+                <div className='icon-container'>
+                    <img src={ZeroIcon} className='icon' alt='ZeroIcon'></img>
                     <h2>
                         Zero Fat
                     </h2>
                 </div>
-                <div>
+                <div className='icon-container'>
+                    <img src={ItalyIcon} className='icon' alt='ItalIcon'></img>
                     <h2>
-                       Italian Import
+                        Italian Import
                     </h2>
+                </div>
+            </div>
+        </div>
+        <div className='divider'>
+            <img src={Divider} alt="DividerImage"></img>
+        </div>
+        <div className='section-3'>
+            <div className='img-container'>
+                <img src={DinoSparkle} className="large-image" alt="DinoLuzziDrink"></img>
+            </div>
+            <div className='right'>
+                <h2 className='section-title'>
+                    Maximum Energy
+                </h2>
+                <div class="info-container">
+                    <h3 className='info-title' >
+                        Vitamin B2
+                    </h3>
+                    <p>
+                        Helps break down fats, proteins, and carbohydrates. B2 plays a vital role in maintaining the body’s energy supply. 
+                    </p>
+                </div>
+                <div class="info-container">
+                    <h3 className='info-title' >
+                        Vitamin B6
+                    </h3>
+                    <p>
+                        Helps with muscle repair. Vitamins are essential micro-nutrients that are required for maintaining normal body functions.
+                    </p>
+                </div>
+                <div class="info-container">
+                    <h3 className='info-title' >
+                        Vitamin B12
+                    </h3>
+                    <p>
+                        Helps keep blood and nerve cells healthy.
+                    </p>
                 </div>
             </div>
         </div>
